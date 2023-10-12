8a62ec0c1e9d76fc1eca16d6567e004bcad85009
samuelsmock <smock.samuel@gmail.com>
2023-10-12 19:10:30 +0200

    October 12th. things working

--- npvCalculator.py
+++ npvCalculator.py
@@ -3,6 +3,7 @@ import geopandas as gpd
 import matplotlib.pyplot as plt
 from plotPrices import plotPrice
 import json
+from tqdm import tqdm
 
 candidateBuildings = gpd.read_file('/Users/sunshinedaydream/Desktop/thesis_data_local/spatial_data/consolidatedThesisData.gpkg', layer = "candidateBuildMatchedCleanedSPF")
 
@@ -11,22 +12,47 @@ energyDict = pd.read_csv('/Users/sunshinedaydream/Desktop/thesis_data_local/non-
 
 priceDict = pd.read_csv('/Users/sunshinedaydream/Desktop/thesis_data_local/non-spatial/codeDictionaries/priceDict.csv')
 
+
 #optionally view the price trajectories being used
 #plotPrice(priceDict)
 
 ### Merge values from energyDict to candidateBuildings based on type ###
 candidateBuildings = candidateBuildings.merge(energyDict, on = 'assigned_type', how = 'left')
-print(len(candidateBuildings))
 
 
-# alert user if the any buildings did not match #
+
+# alert user if the any buildings did not match with energy information from EnergyDict #
 if (len(candidateBuildings[candidateBuildings['SPF'].isnull()]) > 0):
     print(len(candidateBuildings[candidateBuildings['SPF'].isnull()]), " buildings were not matched with energy data")
- 
+
+def plotNPV():
+
+
+    years = pd.to_datetime([num for num in range(2024, 2024 +len(candidateBuildings.loc[0,'scn2_npv']))], format = '%Y')
+
+    plt.plot(years,  candidateBuildings.loc[42, 'scn1_npv'], label='Scenario 1', linewidth = 2.5,)
+    plt.plot(years,  candidateBuildings.loc[42,'scn2_npv'], label='Scenario 2', linewidth = 2.5,)
+    plt.plot(years,  candidateBuildings.loc[42, 'scn3_npv'], label='Scenario 3', linewidth = 2.5,)
+    plt.plot(years,  candidateBuildings.loc[42, 'hist_npv'], label='Pre 2022 Historic Average', linewidth = 2.5,)
+
+    plt.xlim(years[0], years[-1])
+    plt.axhline(y=0, color='black', linestyle='--', linewidth=0.8)
+    # Add labels and legend
+
+    plt.grid(True) 
+    plt.xlabel('Years Past 2024')
+    plt.ylabel(f'NPV @ {round(repDisc *100, 2)}%')
+    plt.title(f'NPV of Air/Water Heatpump in 140 m2 SFH Installed in {2024+start} \n under different Price Trajectories. \n {"Ignoring FF boiler replacement cost" if remLife > 100 else "Taking into account a fossil fuel boiler that needs to be replaced anyway in " + str(remLife) + " years"} \n {("Subsidy: " + str(100*subsidy) + "%")}')
+    plt.legend()
+
+    # Show the plot
+    plt.show()
+
+
 ### on each building loop through the 15 year period to sum the NPV and add it as a column to 
 # candidate buuldings ###
 
-##Returns a series showing the NPV after x number of years.
+##Returns a series showing the NPV after x number of years. (IE it will have an entry for each return period from 0 to 20 in most use cases)
 def npv(building, energyPrices, scenario, discRate, termStart, termEnd, ffBoilerUsefulLife, upfrontSubsidy): ##building variable is a whole row in the buildings dataset
 
     if ffBoilerUsefulLife < termStart or (upfrontSubsidy !=0 & termStart != 0):
@@ -88,8 +114,10 @@ def npv(building, energyPrices, scenario, discRate, termStart, termEnd, ffBoiler
     
     return npvSeries
     #return HPUpfront - ffUpfront + HPOngoing - ffOngoing
-    
-### Initialialize to make it easy to quickly run models
+
+#(npv(candidateBuildings.loc[0], energyDict, 'scn1', 0.028, 1, 20, 100000, 0))
+
+### Each Item in this list represents a specific scenario we would like to investigate ##
 testParameters = [
     {
     'termStart': 0,
@@ -123,22 +151,42 @@ testParameters = [
     'termStart': 0,
     'termEnd': 20,
     'ffBoilerUsefulLife':  10000000000,
+    'upfrontSubsidy': 0.3},
+
+    {
+    'termStart': 20,
+    'termEnd': 20,
+    'ffBoilerUsefulLife':  10000000000,
     'upfrontSubsidy': 0.3}
+
+
 ]
 
 scnList = ['scn1', 'scn2', 'scn3', 'hist']
 
+## This will simply run NPV for all the different scenarios we are interested in and return a JSON object with \
+## keys of (price) Scenarios and values of ordered lists for each potential decision a building owner could make
+
 def scenarioPopulator(bld):
-    result = {} # the result will be a python dictionary later it will be converted to a JSON object
+    result = {} # the result will be a python dictionary, later it will be converted to a JSON object
+    try:
+        for scn in scnList:
+            result[scn] = []
+            for param in testParameters:
+                    result[scn].append(npv(building = bld, discRate = 0.028, scenario= scn, \
+                                           energyPrices = priceDict, **param)[20])
+                    ##index 19 is the NPV after 20 years as the list is 0 indexed
+
+        return json.dumps(result)
     
-    for scn in scnList:
-        result[scn] = []
-        for param in testParameters:
-                result[scn].append(npv(building = bld, discRate = 0.028, scenario= scn, energyPrices = priceDict, **param)[19])
+    except Exception as e:
+        print(f"Problematic Row: {bld}")
+        print(f"Error Message: {e}")
+        return None
 
-    return json.dumps(result)
+tqdm.pandas()
 
-candidateBuildings['npvs'] = candidateBuildings.apply(scenarioPopulator, axis = 1)
+candidateBuildings['npvs'] = candidateBuildings.progress_apply(scenarioPopulator, axis = 1)
 
 print(candidateBuildings.memory_usage(deep=True).sum())
 
@@ -146,28 +194,6 @@ candidateBuildings.to_file('/Users/sunshinedaydream/Desktop/thesis_data_local/sp
 # Plot the columns 'a', 'b', and 'c' on the same graph
 
 
-def plotNPV():
-
-
-    years = pd.to_datetime([num for num in range(2024, 2024 +len(candidateBuildings.loc[0,'scn2_npv']))], format = '%Y')
-
-    plt.plot(years,  candidateBuildings.loc[42, 'scn1_npv'], label='Scenario 1', linewidth = 2.5,)
-    plt.plot(years,  candidateBuildings.loc[42,'scn2_npv'], label='Scenario 2', linewidth = 2.5,)
-    plt.plot(years,  candidateBuildings.loc[42, 'scn3_npv'], label='Scenario 3', linewidth = 2.5,)
-    plt.plot(years,  candidateBuildings.loc[42, 'hist_npv'], label='Pre 2022 Historic Average', linewidth = 2.5,)
-
-    plt.xlim(years[0], years[-1])
-    plt.axhline(y=0, color='black', linestyle='--', linewidth=0.8)
-    # Add labels and legend
-
-    plt.grid(True) 
-    plt.xlabel('Years Past 2024')
-    plt.ylabel(f'NPV @ {round(repDisc *100, 2)}%')
-    plt.title(f'NPV of Air/Water Heatpump in 140 m2 SFH Installed in {2024+start} \n under different Price Trajectories. \n {"Ignoring FF boiler replacement cost" if remLife > 100 else "Taking into account a fossil fuel boiler that needs to be replaced anyway in " + str(remLife) + " years"} \n {("Subsidy: " + str(100*subsidy) + "%")}')
-    plt.legend()
-
-    # Show the plot
-    plt.show()
 
 #plotNPV()
 #candidateBuildings.to_csv('/Users/sunshinedaydream/Desktop/thesis_data_local/spatial_data/scratch/test11.csv')
